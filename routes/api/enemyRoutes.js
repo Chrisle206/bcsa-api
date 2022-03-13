6ee931d703740708a0782434f7d399001547dcd4
Shawn Analla <shawnanalla@gmail.com>
2022-03-13 10:20:49 -0700

    get all enemies route and more seed

--- routes/api/enemyRoutes.js
+++ routes/api/enemyRoutes.js
@@ -5,6 +5,15 @@ const auth = require("../../middleware/auth");
 require('dotenv').config();
 const Enemy = require('../../models/Enemy');
 
+//GET route for acquiring all enemy data, for API purposes. 
+router.get('/enemies', (req, res) => {
+    Enemy.find().then((enemy) =>
+        !enemy
+            ? res.status(404).json({ message: 'No enemy found, check ID' })
+            : res.json(enemy)
+    ).catch((err) => res.status(500).json(err));
+});
+
 //GET route for acquiring enemy data. NOTE: requires JWT token to access. Include JWT token in req.body as 'token.' Refer to middleware/auth
 router.get('/enemy/:id', auth, (req, res) => {
     Enemy.findById(
